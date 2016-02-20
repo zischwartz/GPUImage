789413cd8217ae3b5a249f6a8607cdc3ec3f701a
Zach Schwartz <zischwartz@gmail.com>
2016-02-20 15:31:00 -0500

    works actually!

--- examples/iOS/SimpleSwiftVideoFilterExample/SimpleSwiftVideoFilterExample/ViewController.swift
+++ examples/iOS/SimpleSwiftVideoFilterExample/SimpleSwiftVideoFilterExample/ViewController.swift
@@ -6,11 +6,23 @@ class ViewController: UIViewController {
     var videoCamera:GPUImageVideoCamera?
     var filter:GPUImagePixellateFilter?
     var output:GPUImageRawDataOutput?
-    var bytes:GLubyte?
+    var rawBytesForImage: UnsafeMutablePointer<GLubyte>?
+    
+    //    Long
+    //    http://stackoverflow.com/a/27671003/83859
+
+    //    Short
+    //    http://stackoverflow.com/a/26644308/83859
+
+    //    https://github.com/BradLarson/GPUImage/issues/1905
+
+    let interval = 200
+    var last_time = NSDate()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print("hello world")
+//        Don't forget to deallocate memory
 
         videoCamera = GPUImageVideoCamera(sessionPreset: AVCaptureSessionPreset352x288, cameraPosition: .Back)
         videoCamera!.outputImageOrientation = .Portrait;
@@ -18,14 +30,40 @@ class ViewController: UIViewController {
         videoCamera?.addTarget(filter)
         filter?.addTarget(self.view as! GPUImageView)
         
-        
+//        Works
         output = GPUImageRawDataOutput(imageSize: CGSizeMake(352, 288), resultsInBGRAFormat: true)
+        
         output!.newFrameAvailableBlock = { () in
-            print("hey!")
-            //bytes = output!.rawBytesForImage()
+            print((NSDate().timeIntervalSinceReferenceDate-self.last_time.timeIntervalSinceReferenceDate)*1000)
+            self.rawBytesForImage = self.output!.rawBytesForImage
+            self.last_time = NSDate()
+//            print(self.rawBytesForImage?.memory)
+            
+            let buffer = UnsafeMutableBufferPointer(start: self.rawBytesForImage!, count: Int(352*288*4))
+//            print(buffer[0])
+//            print(buffer[1])
+
+            // WORKS
+//            var data = NSData(bytes: self.rawBytesForImage!, length: Int(352*288*4))
+
+
+            //http://stackoverflow.com/a/31109955/83859
+//            print(data[0])
+//            print(data[1])
+
+//            UTF8String
+//            print(self.rawBytesForImage?.successor().memory)
+//            for b in self.rawBytesForImage!.memory.stride(through: 10, by: 1) {
+//                print(b)
+//            }
+
+            //            print(self.output!.rawBytesForImage.successor().memory)
+
+//            public var rawBytesForImage: UnsafeMutablePointer<GLubyte> { get }
+
         }
         
-        
+
         
         videoCamera?.addTarget(output)
         
@@ -42,10 +80,7 @@ class ViewController: UIViewController {
         
         
 
-//        output?.newFrameReadyAtTime(<#T##frameTime: CMTime##CMTime#>, atIndex: <#T##Int#>)
-//        { (output_2: GPUImageRawDataOutput, error: NSError) in
-//            print("hey")
-//        }
+
 
         //        output     public var newFrameAvailableBlock: (() -> Void)!
 
